4e62dfb91a96025b0970153cd2e59924742c05a8
Jim Rootham <jrootham@gmail.com>
2024-10-01 11:05:57 -0400

    Create site in ship

--- ship.sh
+++ ship.sh
@@ -1,11 +1,13 @@
 #! /usr/bin/bash
 
 # Before running copy the 4 images into the static directory
+# Copy stuff.clj
 
 cd signup/server/signup/
 ./ship.sh $1
 
 cd ../../../
+mkdir -p site
 cp static/* site
 
 cd makesite
